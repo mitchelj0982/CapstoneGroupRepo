775e4c9dc5c627f80f2b245ae7c6fb81a5ce68bd
Gonwokay96 <78820649+Gonwokay96@users.noreply.github.com>
2023-04-30 17:55:02 -0400

    Add files via upload

---
+++ loginPg/src/main/java/com/mycompany/loginpg/LoginPg.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginpg;
+
+/**
+ *
+ * @author Lester
+ */
+public class LoginPg {
+
+    public static void main(String[] args) {
+        loginFrame loginFrame = new loginFrame();
+        loginFrame.setVisible(true);
+    }
+}
