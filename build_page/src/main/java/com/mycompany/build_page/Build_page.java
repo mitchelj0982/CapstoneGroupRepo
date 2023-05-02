9650343b1bdfcffb5d832563e22f40fab8ff2acc
BrandonStrother <55504243+BrandonStrother@users.noreply.github.com>
2023-05-02 19:53:43 -0400

    Add files via upload

---
+++ build_page/src/main/java/com/mycompany/build_page/Build_page.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.build_page;
+
+/**
+ *
+ * @author Brandon
+ */
+public class Build_page {
+
+    public static void main(String[] args) {
+        new build_page_frame().setVisible(true);
+        
+    }
+}
